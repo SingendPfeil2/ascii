1cf9aa6873e5e6e453b4acbf0010cee19cf1aed6
SingendPfeil2 <109229547+SingendPfeil2@users.noreply.github.com>
2024-06-05 16:08:59 +0200

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,4 +1,4 @@
 $batchScriptUrl = 'https://raw.githubusercontent.com/SingendPfeil2/ascii/main/index.bat'
 $batchScriptPath = "$env:TEMP\index.bat"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SingendPfeil2/ascii/main/index.bat -OutFile $env:TEMP\index.bat
-Start-Process 'cmd.exe' "/c $env:TEMP\index.bat"
+Start-Process 'cmd.exe' "/c $env:TEMP\index.bat" -NoNewWindow -wait
