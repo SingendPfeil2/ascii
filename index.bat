2208f1bf6feff26b4bd2918f131b00bcc34e8719
SingendPfeil2 <109229547+SingendPfeil2@users.noreply.github.com>
2024-06-05 12:38:17 +0200

    Update index.bat

--- index.bat
+++ index.bat
@@ -1,3 +1,25 @@
-echo test
 @echo off
-echo test #2
+setlocal
+
+:input
+set "valid_input=forrest parrot clock nyan rick can-you-hear-me donut"
+set "user_input="
+
+rem Prompt the user for input
+set /p "user_input=Enter one of the following options (forrest, parrot, clock, nyan, rick, can-you-hear-me, donut): "
+
+rem Check if the input is valid
+for %%i in (%valid_input%) do (
+    if /i "%user_input%"=="%%i" (
+        set "valid=1"
+        goto :valid_input
+    )
+)
+
+echo Invalid input. Exiting...
+goto :eof
+
+:valid_input
+curl ascii.live/%user_input%
+
+endlocal
