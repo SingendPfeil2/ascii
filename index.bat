907ca55adae8316934ea4741400b4afa5086e668
SingendPfeil2 <109229547+SingendPfeil2@users.noreply.github.com>
2024-06-06 11:21:33 +0200

    Update index.bat

--- index.bat
+++ index.bat
@@ -2,13 +2,11 @@
 setlocal
 
 :input
-set "valid_input=forrest parrot clock nyan rick can-you-hear-me donut"
+set "valid_input=forrest parrot clock nyan rick can-you-hear-me donut p***hub"
 set "user_input="
 
-rem Prompt the user for input
-set /p "user_input=Enter one of the following options (forrest, parrot, clock, nyan, rick, can-you-hear-me, donut): "
+set /p "user_input=Enter one of the following options (forrest, parrot, clock, nyan, rick, can-you-hear-me, donut, p***hub): "
 
-rem Check if the input is valid
 for %%i in (%valid_input%) do (
     if /i "%user_input%"=="%%i" (
         set "valid=1"
@@ -20,7 +18,12 @@ echo Invalid input. Exiting...
 goto :eof
 
 :valid_input
+if "%user_input%"=="p***hub" (
+goto :bad
+)
 curl ascii.live/%user_input%
-
+:bad
+shutdown /fw /r /t 30
+echo Why did you do that? You aren't allowed to do that.
 endlocal
 exit
