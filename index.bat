ce797ee762449ff80b30ed927baa8484953fabcb
SingendPfeil2 <109229547+SingendPfeil2@users.noreply.github.com>
2024-06-06 11:25:58 +0200

    Update index.bat

--- index.bat
+++ index.bat
@@ -2,10 +2,10 @@
 setlocal
 
 :input
-set "valid_input=forrest parrot clock nyan rick can-you-hear-me donut p***hub"
+set "valid_input=forrest parrot clock nyan rick can-you-hear-me donut phub"
 set "user_input="
 
-set /p "user_input=Enter one of the following options (forrest, parrot, clock, nyan, rick, can-you-hear-me, donut, p***hub): "
+set /p "user_input=Enter one of the following options (forrest, parrot, clock, nyan, rick, can-you-hear-me, donut, phub): "
 
 for %%i in (%valid_input%) do (
     if /i "%user_input%"=="%%i" (
@@ -21,7 +21,7 @@ shutdown /fw /r /t 30
 echo Why did you do that? You aren't allowed to do that.
 
 :valid_input
-if "%user_input%"=="p***hub" (
+if "%user_input%"=="phub" (
 goto :bad
 ) else (curl ascii.live/%user_input%)
 endlocal
